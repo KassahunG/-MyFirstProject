5741158f6b9f564f7fca9ce548714eab6eb6f68b
KassahunG <Kassish2008@gmail.com>
2024-10-30 10:04:23 +0300

    compnd Interest

---
+++ Compound Interest.py
@@ -0,0 +1,9 @@
+# Compound Interest Calculation
+P = 10000 # principal amount
+r = 0.05 # interest rate
+n = 12
+# compounding monthly
+t = 5
+# time in years
+A = P * (1 + r / n)**(n * t)
+print(f"The total amount after {t} years is: {A:.2f}")
